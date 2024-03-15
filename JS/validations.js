c35d07b29c85b7a3a1159901817faf9536a4c3ac
MarvinCornejo17 <157768968+MarvinCornejo17@users.noreply.github.com>
2024-03-14 17:19:27 -0700

    Mas validaciones
    Se realizaron las validaciones de login modificacion y registro

--- JS/validations.js
+++ JS/validations.js
@@ -26,7 +26,8 @@ $(document).ready(function() {
                 swal("Operación cancelada", "No ingresaste el código de verificación.", "error");
             } else if (codigoIngresado === codigo.toString()) {
                 // El código ingresado coincide con el código generado
-                swal("Éxito", "El código es correcto. Puedes continuar con el proceso de recuperación de contraseña.");
+                swal("¡Éxito!", "El código es correcto. Puedes continuar con el proceso de recuperación de contraseña.", "success");
+                window.location.href = "mod-usuario.html";
             } else {
                 // El código ingresado no coincide con el código generado
                 swal("Error", "El código ingresado no es válido.", "error");
@@ -55,7 +56,121 @@ $(document).ready(function() {
         } else if (!expCorreo.test(correo)) {
             swal("Error...", "Correo electrónico inválido", "error");
         } else {
-            swal("Comentario enviado con éxito");
+            swal("¡Exito!", "Tu comentario enviado con éxito", "success");
         }
     });
 });
+
+$(document).ready(function() {
+    $('#registrar').click(function(event){
+        var nombre = $('#nombre').val().trim();
+        var apellido = $('#apellido').val().trim();
+        var correo = $('#correo').val().trim();
+        var contrasena = $('#contrasena').val();
+        var confirmarContrasena = $('#confirmarContrasena').val();
+        var captchaResponse = grecaptcha.getResponse(); // Obtiene la respuesta del reCAPTCHA
+        var expCorreo = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+        var expNombre = /^[a-zA-Z]+$/; // Expresión regular para permitir solo letras en el nombre
+        var expContrasena = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
+
+        if (nombre.length === 0 || apellido.length == 0 || correo.length === 0 || contrasena.length === 0 || confirmarContrasena.length === 0) {
+            swal("Error...", "No debe haber campos vacíos", "error");
+        } else if (!expNombre.test(nombre)) {
+            swal("Error...", "El nombre solo puede contener letras", "error");
+        } else if (!expNombre.test(apellido)) {
+            swal("Error...", "El apellido solo puede contener letras", "error");
+        } else if (nombre.length > 30) {
+            swal("Error...", "Nombre demasiado largo", "error");
+        } else if (apellido.length > 30) {
+            swal("Error...", "Apellido demasiado largo", "error");
+        } else if (correo.length > 50) {
+            swal("Error...", "Correo electrónico demasiado largo", "error");
+        } else if (!expCorreo.test(correo)) {
+            swal("Error...", "Correo electrónico inválido", "error");
+        } else if (!expContrasena.test(contrasena)) {
+            swal("Error...", "La contraseña debe contener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial", "error");
+        } else if (contrasena !== confirmarContrasena) {
+            swal("Error...", "Las contraseñas no coinciden", "error");
+        } else {
+            // Verifica si el reCAPTCHA se ha completado
+            if (captchaResponse) {
+                swal("¡Registro exitoso!", "Tus datos han sido registrados de manera exitosa", "success");
+                window.location.href = "Index.html";
+            } else {
+                swal("Error...", "Por favor, complete el reCAPTCHA", "error");
+            }
+        }
+    });
+});
+
+$(document).ready(function() {
+    $('#login').click(function(event){
+        var correo = $('#correo').val().trim();
+        var contrasena = $('#contrasena').val();
+        var captchaResponse = grecaptcha.getResponse(); // Obtiene la respuesta del reCAPTCHA
+        var expCorreo = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+        var expContrasena = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
+
+        if (correo.length === 0 || contrasena.length === 0 ) {
+            swal("Error...", "No debe haber campos vacíos", "error");
+        } else if (correo.length > 50) {
+            swal("Error...", "Correo electrónico demasiado largo", "error");
+        } else if (!expCorreo.test(correo)) {
+            swal("Error...", "Correo electrónico inválido", "error");
+        } else if (!expContrasena.test(contrasena)) {
+            swal("Error...", "Contraseña Invalida", "error");
+        } else {
+            // Verifica si el reCAPTCHA se ha completado
+            if (captchaResponse) {
+                swal("¡Registro exitoso!", "Se ha iniciado sesion correctamente", "success");
+                window.location.href = "mod-usuario.html";
+            } else {
+                swal("Error...", "Por favor, complete el reCAPTCHA", "error");
+            }
+        }
+    });
+});
+
+$(document).ready(function() {
+    $('#modificar').click(function(event){
+        var nombre = $('#nombre').val().trim();
+        var apellido = $('#apellido').val().trim();
+        var correo = $('#correo').val().trim();
+        var contrasena = $('#contrasena').val();
+        var confirmarContrasena = $('#confirmarContrasena').val();
+        var captchaResponse = grecaptcha.getResponse(); // Obtiene la respuesta del reCAPTCHA
+        var expCorreo = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+        var expNombre = /^[a-zA-Z]+$/; // Expresión regular para permitir solo letras en el nombre
+        var expContrasena = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
+
+        if (nombre.length === 0 || apellido.length == 0 || correo.length === 0 || contrasena.length === 0 || confirmarContrasena.length === 0) {
+            swal("Error...", "No debe haber campos vacíos", "error");
+        } else if (!expNombre.test(nombre)) {
+            swal("Error...", "El nombre solo puede contener letras", "error");
+        } else if (!expNombre.test(apellido)) {
+            swal("Error...", "El apellido solo puede contener letras", "error");
+        } else if (nombre.length > 30) {
+            swal("Error...", "Nombre demasiado largo", "error");
+        } else if (apellido.length > 30) {
+            swal("Error...", "Apellido demasiado largo", "error");
+        } else if (correo.length > 50) {
+            swal("Error...", "Correo electrónico demasiado largo", "error");
+        } else if (!expCorreo.test(correo)) {
+            swal("Error...", "Correo electrónico inválido", "error");
+        } else if (!expContrasena.test(contrasena)) {
+            swal("Error...", "La contraseña debe contener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial", "error");
+        } else if (contrasena !== confirmarContrasena) {
+            swal("Error...", "Las contraseñas no coinciden", "error");
+        } else {
+            // Verifica si el reCAPTCHA se ha completado
+            if (captchaResponse) {
+                swal("¡Modificacion exitosa!", "Tus datos han sido modificados correctamente", "success");
+            } else {
+                swal("Error...", "Por favor, complete el reCAPTCHA", "error");
+            }
+        }
+    });
+});
+
+
+
